32f0221741e29dddeccf77ab02e79e8a2aebb8ed
Gabriel Freitas <35998400+GabrielFre74@users.noreply.github.com>
2019-12-05 12:21:57 -0300

    Delete Aluno.py

--- EscolaServicoApp/Entidades/Aluno.py
+++
@@ -1,8 +0,0 @@
-class Aluno():
-    def __init__(self, nome, matricula, cpf, nascimento, id_endereco, id_curso):
-        self.nome = nome
-        self.matricula = matricula
-        self.cpf = cpf
-        self.nascimento = nascimento
-        self.id_endereco = id_endereco
-        self.id_curso = id_curso
