f46aa8989a1f0c4c62bb6923f35f3588ddd37dfd
Gabriel Freitas <35998400+GabrielFre74@users.noreply.github.com>
2019-12-05 12:22:23 -0300

    Delete Endereco.py

--- EscolaServicoApp/Entidades/Endereco.py
+++
@@ -1,7 +0,0 @@
-class Endereco():
-    def __init__(self, logradouro, complemento, bairro, cep, numero):
-        self.logradouro = logradouro
-        self.complemento = complemento
-        self.bairro = bairro
-        self.cep = cep
-        self.numero = numero
