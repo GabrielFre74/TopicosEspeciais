4c51a4e7aa17bd977aa5bbe3a1d92595a3aecd0d
IFPB <gabrielfre74@gmail.com>
2019-12-05 12:30:51 -0300

    Serviço

---
+++ EscolaServicoApp/Entidades/Aluno.py
@@ -0,0 +1,8 @@
+class Aluno():
+    def __init__(self, nome, matricula, cpf, nascimento, id_endereco, id_curso):
+        self.nome = nome
+        self.matricula = matricula
+        self.cpf = cpf
+        self.nascimento = nascimento
+        self.id_endereco = id_endereco
+        self.id_curso = id_curso
---
+++ EscolaServicoApp/Entidades/Curso.py
@@ -0,0 +1,4 @@
+class Curso():
+    def __init__(self, nome, id_turno):
+        self.nome = nome
+        self.id_turno = id_turno
